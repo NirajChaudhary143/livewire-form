5e9a64263216a8dbf9e2832495e2209bed8c7ad7
NirajChaudhary143 <nirajchy2000@gmail.com>
2023-06-09 15:20:37 +0545

    form-validation

--- app/Http/Livewire/Registration.php
+++ app/Http/Livewire/Registration.php
@@ -10,6 +10,14 @@ class Registration extends Component
     public function submit(){
         dd($this->name,$this->email,$this->phone_number);
     }
+// Form Validation
+    public function updated($field){
+        $this->validateOnly($field,[
+            'name'=>['required', 'not_regex:/^[0-9]+$/'],
+            'email'=>'required|email',
+            'phone_number'=>'required|numeric|digits:10'
+        ]);
+    }
     public function render()
     {
         return view('livewire.registration');
--- resources/views/livewire/registration.blade.php
+++ resources/views/livewire/registration.blade.php
@@ -2,10 +2,13 @@
     <h1>Registration</h1>
     <form wire:submit.prevent="submit">
         <input type="text" placeholder="Enter Name" wire:model="name">
+        @error('name') {{$message}} @enderror
         <br><br>
         <input type="email" placeholder="Enter email" wire:model="email">
+        @error('email') {{$message}} @enderror
         <br><br>
         <input type="text" placeholder="Enter Phone Number" wire:model="phone_number">
+        @error('phone_number') {{$message}} @enderror
         <br><br>
        <input type="submit">
     </form>
