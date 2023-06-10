85717ce9019a6f2c1b3f9e4a20ae893b9a02697a
NirajChaudhary143 <nirajchy2000@gmail.com>
2023-06-10 06:00:50 +0545

    insert-data-and-reset-input-fields

--- app/Http/Livewire/Registration.php
+++ app/Http/Livewire/Registration.php
@@ -3,20 +3,38 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Student;
 
 class Registration extends Component
 {
     public $name,$email,$phone_number;
     public function submit(){
-        dd($this->name,$this->email,$this->phone_number);
+        // $this->validate([
+        //     'name'=>['required', 'not_regex:/^[0-9]+$/'],
+        //     'email'=>'required|email|unique:students,email',
+        //     'phone_number'=>'required|numeric|digits:10'
+        // ]);
+        $student = new Student();
+        $student->name = $this->name;
+        $student->email = $this->email;
+        $student->phone_number = $this->phone_number;
+        $student->save();
+        // We need to reset the form field
+        $this->resetFields();
+
     }
-// Form Validation
+// Reset the input fields
+    public function resetFields(){
+        $this->reset(['name','email','phone_number']);
+    }
+// Form Validation Live
     public function updated($field){
         $this->validateOnly($field,[
             'name'=>['required', 'not_regex:/^[0-9]+$/'],
-            'email'=>'required|email',
+            'email'=>'required|email|unique:students,email',
             'phone_number'=>'required|numeric|digits:10'
         ]);
+       
     }
     public function render()
     {
